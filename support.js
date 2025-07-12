bd1e05caeeb56285688dbe0a2d41e56c4ff582cd
OPELMOARRRRR <168326091+faggggg123459@users.noreply.github.com>
2025-07-12 19:44:36 +0300

    Update support.js

--- support.js
+++ support.js
@@ -1,1 +1,13 @@
+function sendMessage(event) {
+  event.preventDefault();
+  const msg = document.getElementById("responseMsg");
+  msg.textContent = "✅ Спасибо! Ваше сообщение отправлено.";
+  msg.style.color = "green";
+}
 
+function submitComplaint(event) {
+  event.preventDefault();
+  const msg = document.getElementById("complaintMsg");
+  msg.textContent = "✅ Ваша жалоба успешно зарегистрирована.";
+  msg.style.color = "darkred";
+}
